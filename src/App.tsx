9a1ba3e0617a1dcaca0d2f48b004264d509bfd02
Oskars <oscar.koleda@gmail.com>
2025-03-30 16:21:15 +0300

    project created

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Box, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <Typography variant="h5" component="div">
+        Hello World
+      </Typography>
+    </Box>
+  );
+}
+
+export default App;
